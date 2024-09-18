a581f29accfc046d2a131708b36bc9039e16a8a1
Isaiah McDaniel <ike45mc@gmail.com>
2024-09-18 15:09:06 -0500

    CSV will only be generated if file name is specified.

--- gtfs_feeds.py
+++ gtfs_feeds.py
@@ -8,21 +8,26 @@ import requests
 import pandas as pd
 
 def fetch(url, file_name):
+    # to hold the current working directory to save the csv file.
     cwd = Path.cwd()
 
     # connects to the realtime feed and parses information
     feed = gtfs_realtime_pb2.FeedMessage()
     response = requests.get(url, timeout=100)
     feed.ParseFromString(response.content)
+
     # uses the protobuf package to compile the feed into a dictionary
     transit_dict = protobuf_to_dict(feed)
 
     # normalizes the returned json and then adds it to a csv file
-    # prints all the information
     try:
         file = pd.json_normalize(transit_dict['entity'])
-        file.to_csv(path_or_buf = f"{cwd}\{file_name}.csv")
+
+        # creates the csv file if a name for the file is given.
+        if file_name: 
+            file.to_csv(path_or_buf = f"{cwd}\{file_name}.csv")
         return file
+    
     except Exception as error:
         print(error)
         pass
--- vehicles.py
+++ vehicles.py
@@ -6,14 +6,17 @@ from pprint import pprint
 import sql_connection
 
 class VehicleData:
-    def __init__(self, file= "vehicle_positions") -> None:
+    def __init__(self, file=None) -> None:
         self.vhpos_path = "http://transitdata.nashvillemta.org/TMGTFSRealTimeWebService/vehicle/vehiclepositions.pb"
         self.file = file
         self.gdf = None
+        self.update_vhpos()
 
     def update_vhpos(self):
-
+        # loads information to DataFrame
         df = fetch(self.vhpos_path, self.file)
+
+        # converts pandas DataFrame to GeoDataFrame
         self.gdf = geopandas.GeoDataFrame(df,geometry=geopandas.points_from_xy(df["vehicle.position.latitude"], df["vehicle.position.longitude"]), crs="EPSG:4326")
 
     # prints the gdf if non empty
@@ -23,7 +26,7 @@ class VehicleData:
         else:
             print("GeoDataFrame is empty")
 
+    # adds the GeoDatabase to Postgresql as a PostGIS database
 
 vh = VehicleData()
-vh.update_vhpos()
 vh.print_gdf()
