4d6c7ef748b49d208b523e598a75536920bcccd1
Isaiah McDaniel <ike45mc@gmail.com>
2025-04-05 13:50:46 -0500

    Changed __init__.py file to allow better imports

---
+++ data/__init__.py
@@ -0,0 +1,3 @@
+from .gtfs_trip_updates import TripUpdate, TripDescriptor, StopTimeUpdate, StopTimeEvent, VehicleDescriptor
+from .gtfs_vehicle_positions import VehiclePosition, Position
+from .gtfs_service_alerts import ServiceAlert
